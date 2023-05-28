a3a47ed941f453eef9156d4787c0e0da38c0f36a
Filip Schwarz <filipschwarz007@gmail.com>
2023-05-28 15:18:35 +0200

    init backend

---
+++ src/main/kotlin/com/flux/filmtracker/FilmTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.flux.filmtracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilmTrackerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FilmTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/flux/filmtracker/FilmTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.flux.filmtracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilmTrackerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
