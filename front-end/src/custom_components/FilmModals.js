194a02a6b16d55460031db772768c3e8d07454c7
Flaxar <filipschwarz007@gmail.com>
2023-06-14 13:48:38 +0200

    added placeholder card and new modal

---
+++ front-end/src/custom_components/FilmModals.js
@@ -0,0 +1,77 @@
+
+
+export function FilmModal(props) {
+    if(props.visible) {
+        return (
+            <div id="defaultModal" tabIndex="-1" aria-hidden="true"
+                 className="fixed flex justify-center align-middle z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div className="relative w-full max-w-2xl max-h-full">
+                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                        <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                Add new film
+                            </h3>
+                            <button type="button" onClick={props.closeFunction}
+                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="defaultModal">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+
+                        <div className="p-6 space-y-6">
+                            <div className="flex items-center justify-center w-full">
+                                <label htmlFor="dropzone-file"
+                                       className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none"
+                                             stroke="currentColor" viewBox="0 0 24 24"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+                                        </svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                            className="font-semibold">Upload film picture</span></p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">Drag and drop / click to upload</p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF
+                                            (MAX. 800x400px)</p>
+                                    </div>
+                                    <input id="dropzone-file" type="file" className="hidden"/>
+                                </label>
+                            </div>
+
+
+                            <div id="filmInputFieleds">
+                                <label htmlFor="helper-text"
+                                       className="text-base text-gray-900 dark:text-white font-bold">Film name</label>
+                                <input id="helper-text" aria-describedby="helper-text-explanation"
+                                       className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="Lord of the Rings, Oldboy, ..."/>
+                            </div>
+
+                        </div>
+
+                        <div
+                            className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button data-modal-hide="defaultModal" type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                                accept
+                            </button>
+                            <button data-modal-hide="defaultModal" type="button" onClick={props.closeFunction}
+                                    className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
+        return null;
+    }
+}
--- src/main/kotlin/com/flux/filmtracker/FilmTrackerApplication.kt
+++ src/main/kotlin/com/flux/filmtracker/FilmTrackerApplication.kt
@@ -2,9 +2,18 @@ package com.flux.filmtracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FilmTrackerApplication
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello";
+}
 
 fun main(args: Array<String>) {
     runApplication<FilmTrackerApplication>(*args)
